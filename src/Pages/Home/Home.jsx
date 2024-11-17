6d899c99409ae8bf449148579a4226d94e3578b6
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-11-17 17:50:14 +0530

    filter added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Alert, Badge, Box, Chip, Divider, IconButton, Snackbar, SwipeableDrawer, Typography, styled } from '@mui/material'
+import { Alert, Badge, Box, Button, Checkbox, Chip, Divider, FormControlLabel, FormGroup, Grid2, IconButton, Radio, RadioGroup, Slider, Snackbar, SwipeableDrawer, Typography, styled } from '@mui/material'
 import SearchBar from '../../Components/SearchBar';
 import FilterCard from '../../Components/FilterCard';
 import PopularCard from '../../Components/PopularCard';
@@ -9,6 +9,9 @@ import { useState } from 'react';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import { FilterOptions, MenuFoods } from '../../Utils/SupportFunctions';
 import { useNavigate } from 'react-router-dom';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types';
 
 function Home() {
     const navigation = useNavigate()
@@ -63,6 +66,28 @@ function Home() {
             openFilterDrawer: state
         }))
     }
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const [sliderValue, setSliderValue] = useState([20, 37]);
+
+    const handleSliderChange = (event, newValue) => {
+        setSliderValue(newValue);
+    };
+
+    const marks = [
+        {
+            value: 0,
+            label: '0',
+        },
+        {
+            value: 5000,
+            label: '5000',
+        },
+    ];
 
     return (
         <>
@@ -180,8 +205,8 @@ function Home() {
                 <Typography variant='h3' sx={{ p: '16px' }}>
                     Filter
                 </Typography>
-                <Divider />
-                <Typography sx={{ px: '16px', pt: '8px' }}>Selected Filters</Typography>
+                {/* <Divider /> */}
+                {/* <Typography sx={{ px: '16px', pt: '8px' }}>Selected Filters</Typography> */}
                 <VerticalScrollContainer>
                     <Chip
                         label="Filter"
@@ -189,6 +214,76 @@ function Home() {
                         onClick={() => handleFilterDrawer(true)}
                     />
                 </VerticalScrollContainer>
+                <Divider />
+                <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}>
+                    <Tabs
+                        orientation="vertical"
+                        variant="scrollable"
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="Vertical tabs example"
+                        sx={{ borderRight: 1, borderColor: 'divider' }}
+                    >
+                        <Tab label="Catogery" {...a11yProps(0)} />
+                        <Tab label="rating" {...a11yProps(1)} />
+                        <Tab label="Veg/Non" {...a11yProps(2)} />
+                        <Tab label="Amount" {...a11yProps(3)} />
+                        <Tab label="Sort" {...a11yProps(4)} />
+                    </Tabs>
+                    <TabPanel value={value} index={0}>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label="All" />
+                            <FormControlLabel control={<Checkbox />} label="Burger" />
+                            <FormControlLabel control={<Checkbox />} label="Biriyani" />
+                        </FormGroup>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label="4 ★ and Above" />
+                            <FormControlLabel control={<Checkbox />} label="3 ★ and Above" />
+                            <FormControlLabel control={<Checkbox />} label="2 ★ and Above" />
+                        </FormGroup>
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label="Veg" />
+                            <FormControlLabel control={<Checkbox />} label="NonVeg" />
+                        </FormGroup>
+                    </TabPanel>
+                    <TabPanel value={value} index={3}>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label="Under Rs 1000" />
+                            <FormControlLabel control={<Checkbox />} label="Rs 1000 to Rs 500" />
+                            <FormControlLabel control={<Checkbox />} label="Rs 499 to Rs 0" />
+                            <Slider
+                                getAriaLabel={() => 'Temperature range'}
+                                value={sliderValue}
+                                onChange={handleSliderChange}
+                                valueLabelDisplay="auto"
+                                min={0}
+                                max={5000}
+                                marks={marks}
+                            />
+                        </FormGroup>
+                    </TabPanel>
+                    <TabPanel value={value} index={4}>
+                        <RadioGroup name="use-radio-group" defaultValue="first">
+                            <FormControlLabel value="price" control={<Radio />} label="Price" />
+                            <FormControlLabel value="rating" control={<Radio />} label="Rating" />
+                        </RadioGroup>
+                    </TabPanel>
+                </Box>
+                <Divider />
+                <Box sx={{ p: '16px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item size={6}>
+                            <Button variant="outlined" sx={{ width: '100%' }}>Clear all</Button>
+                        </Grid>
+                        <Grid item size={6}>
+                            <Button variant="contained" sx={{ width: '100%' }}>Filter</Button>
+                        </Grid>
+                    </Grid>
+                </Box>
             </SwipeableDrawer>
             <Snackbar
                 open={snackbar.open}
@@ -242,6 +337,7 @@ const MenuContainer = styled(Box)(() => ({
     flexDirection: 'column',
     gap: '16px'
 }))
+const Grid = styled(Grid2)(() => ({}))
 
 const Puller = styled('div')(({ theme }) => ({
     width: 30,
@@ -255,3 +351,37 @@ const Puller = styled('div')(({ theme }) => ({
         backgroundColor: grey[900],
     }),
 }));
+
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
